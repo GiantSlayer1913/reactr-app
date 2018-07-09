8e227abff9c573f1f9bf4a4724cc25b82ca8d7ec
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-07-09 16:44:48 -0400

    Added form js app js code and css code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './form.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  state = {
+    fields: {}
   }
-}
+
+  onChange = (updatedValue) => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    })
+  }
+
+
+  render() {
+
+    const title = "Welcome to Reactr App";
+
+    return (
+      // <MultiThemeProvider>
+        <div className="App">
+          <h1>{title}</h1>
+          <Form onChange={fields => this.onChange(fields)}/>
+            <p>{JSON.stringify(this.state.fields, null, 2)}
+            </p>
+        </div>
+      // </MultiThemeProvider>
+
+      );
+    }
+  }
 
 export default App;
+
+
+// --- TEMPLATE Component ----
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
---
+++ src/form.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+// import { AwesomeButton } from 'react-awesome-button';
+// import 'react-awesome-button/dist/styles.css';
+
+// Form Component
+class Form extends React.Component {
+
+  // FORM state
+  state = {
+    firstName: "",
+    firstNameError: "",
+    lastName: "",
+    lastNameError: "",
+    username: "",
+    userNameError: "",
+    email: "",
+    emailError: "",
+    password: "",
+    passwordError: ""
+  }
+
+// Change method
+  change = (event) => {
+    this.props.onChange({ [event.target.name]: event.target.value })
+    this.setState({
+    [event.target.name]: event.target.value
+    })
+  }
+
+// Validates form inputs are correct
+  validate = () => {
+    let isError = false;
+      const errors = {
+        firstNameError: "",
+        lastNameError: "",
+        userNameError: "",
+        emailError: "",
+        passwordError: ""
+      }
+
+      if (this.state.firstName.length === 0) {
+        isError = true;
+        errors.firstNameError = "Please enter your first name"
+      }
+
+      if (this.state.lastName.length === 0) {
+        isError = true;
+        errors.lastNameError = "Please enter your last name"
+      }
+
+      if (this.state.username.length < 6) {
+        isError = true;
+        errors.userNameError = "Username needs to be at least 6 charachters long"
+      }
+
+      if (this.state.email.indexOf("@") === -1) {
+        isError = true;
+        errors.emailError = "Requires valid email address"
+      }
+
+
+      this.setState({
+        ...this.state,
+        ...errors
+      })
+
+      return isError
+  }
+
+// Submit method
+  onSubmit = (event) => {
+    event.preventDefault()
+    // this.props.onSubmit(this.state)
+    const err = this.validate()
+    if (!err)
+    this.setState({
+      firstName: "",
+      firstNameError: "",
+      lastName: "",
+      lastNameError: "",
+      username: "",
+      usernameError: "",
+      email: "",
+      emailError: "",
+      password: "",
+      passwordError: ""
+    })
+
+    this.props.onChange({
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: ""
+    })
+  }
+
+// Render Component
+  render() {
+    return (
+      <form>
+        <label>
+        <input
+          name="firstName"
+          placeholder="First Name"
+          // hintText="First Name"
+          // floatingLabelText="First Name"
+          value={this.state.firstName}
+          onChange={event => this.change(event)}
+          errorText={this.state.firstNameError}
+          // floatingLabelFixed
+          />
+          <br />
+
+        <input
+          name="lastName"
+          placeholder="Last Name"
+          // hintText="Last Name"
+          // floatingLabelText="Last Name"
+          value={this.state.lastName}
+          onChange={event => this.change(event)}
+          errorText={this.state.lastNameError}
+          // floatingLabelFixed
+        />
+        <br />
+
+        <input
+          name="username"
+          placeholder="Username"
+          // hintText="Username"
+          // floatingLabelText="Username"
+          value={this.state.username}
+          onChange={event => this.change(event)}
+          errorText={this.state.userNameError}
+          // floatingLabelFixed
+        />
+        <br />
+
+        <input
+          name="email"
+          placeholder="Email"
+          // hintText="Email"
+          // floatingLabelText="Email"
+          value={this.state.email}
+          onChange={event => this.change(event)}
+          errorText={this.state.emailError}
+          // floatingLabelFixed
+        />
+        <br />
+
+        <input
+          name="password"
+          placeholder="Password"
+          // hintText="Password"
+          // floatingLabelText="Password"
+          value={this.state.password}
+          onChange={event => this.change(event)}
+          errorText={this.state.passwordError}
+          type="password"
+          // floatingLabelFixed
+        />
+        </label>
+        <br />
+        
+      <button label="Submit" onClick={event => this.onSubmit(event)} type="primary">Submit</button>
+    </form>
+    )
+  }
+}
+
+export default Form;
